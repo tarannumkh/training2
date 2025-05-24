965d16991563ead95a1c8f5e7ad9e5d98003805b
=Ruchee Singh <=ruchisingh7915@gmail.com>
2025-05-24 11:15:27 +0545

    home page chages

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-lksjd lskdjf lskdjf lskdfj slkdfj slkdfj sdwq
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Welcome the changes
+</body>
+</html>
